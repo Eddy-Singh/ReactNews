96bb1d6c4601780b079cbf1df086811e85351719
Eddy-Singh <appsingh@mun.ca>
2024-06-22 11:27:33 -0230

    Fetch top headlines from News API on component mount

--- src/components/News.js
+++ src/components/News.js
@@ -1663,16 +1663,25 @@ export class News extends Component {
 
     this.state = { articles: this.articles, loading: false };
   }
+
+  componentDidMount = async () => {
+    console.log("Component Did Mount");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=1009d5eaee264c65a90cc70db1ac975b`;
+    let response = await fetch(url);
+    let data = await response.json();
+    this.setState({ articles: data.articles });
+  };
+
   render() {
     return (
       <div className="container my-3">
         <h2>ReactNews- Top Headlines</h2>
         <div className="row">
-          {this.articles.map((element) => {
+          {this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
-                  title={element.title.slice(0, 45)}
+                  title={element.title ? element.title.slice(0, 45) : ""}
                   description={
                     element.description
                       ? element.description.length > 120
@@ -1680,7 +1689,11 @@ export class News extends Component {
                         : element.description
                       : ""
                   }
-                  imageUrl={element.urlToImage}
+                  imageUrl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frealitypaper.com%2Fwp-content%2Fuploads%2F2020%2F08%2FCommon-Types-of-News-to-Explore.jpg&f=1&nofb=1&ipt=5f46f041897df3a9b4bed0da7f07810fe5db2c6754970ff28f863db3fa4a5fda&ipo=images"
+                  }
                   newsUrl={element.url}
                 />
               </div>
