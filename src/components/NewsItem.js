89eba058dcf6b8cf9c73222b683769470610ee74
Eddy-Singh <appsingh@mun.ca>
2024-06-30 20:28:19 -0230

    refactor: Convert NewsItem component to a functional component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,50 +1,46 @@
-import React, { Component } from "react";
-
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <img
-            src={imageUrl ? imageUrl : "https://via.placeholder.com/1000"}
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">
-              {title}{" "}
-              <span
-                className="position-absolute top-0 start-100 badge rounded-pill bg-danger"
-                style={{
-                  transform: "translateX(-100%) translateY(-50%)",
-                  zIndex: 1,
-                }}
-              >
-                {source}
-              </span>
-            </h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small class="text-body-secondary">
-                by {author} on {new Date(date).toUTCString()}
-              </small>
-            </p>
-
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
+import React from "react";
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <img
+          src={imageUrl ? imageUrl : "https://via.placeholder.com/1000"}
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">
+            {title}{" "}
+            <span
+              className="position-absolute top-0 start-100 badge rounded-pill bg-danger"
+              style={{
+                transform: "translateX(-100%) translateY(-50%)",
+                zIndex: 1,
+              }}
             >
-              Read More
-            </a>
-          </div>
+              {source}
+            </span>
+          </h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small class="text-body-secondary">
+              by {author} on {new Date(date).toUTCString()}
+            </small>
+          </p>
+
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
